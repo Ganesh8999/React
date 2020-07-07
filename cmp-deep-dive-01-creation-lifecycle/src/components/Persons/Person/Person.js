a8209bbaa774095cabafd54ed7422672cfd8faba
Ganesh8999 <Ganesh8999@github.com>
2020-07-07 11:37:25 +0530

    using PropTypes

--- cmp-deep-dive-01-creation-lifecycle/src/components/Persons/Person/Person.js
+++ cmp-deep-dive-01-creation-lifecycle/src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from "prop-types"
 
 import classes from './Person.css';
 
@@ -16,5 +17,13 @@ class Person extends Component {
     );
   }
 };
+
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
+}
 
 export default Person;
