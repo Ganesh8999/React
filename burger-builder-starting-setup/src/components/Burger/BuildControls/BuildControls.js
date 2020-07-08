ecceec9208419ba72098852ec905e7763357b96f
Ganesh8999 <Ganesh8999@github.com>
2020-07-08 14:50:29 +0530

    outputting build controls

--- burger-builder-starting-setup/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder-starting-setup/src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,23 @@
 import React from "react";
+import classes from "./BuildControls.css"
+import BuildControl from "./BuildControl/BuildControl"
+
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Meat', type: 'meat' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+
+]
 
 const buildControls = (props) => (
 
-    <div></div>
+    <div className={classes.BuildControls}>
+
+        {controls.map(cntrl => (
+            <BuildControl key={cntrl.label} label={cntrl.label} />
+        ))}
+    </div>
 
 );
 
