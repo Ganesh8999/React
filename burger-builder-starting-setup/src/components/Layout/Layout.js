046e66ac6e5c53f6ec24c3022a4da41a7bd357f9
Ganesh8999 <Ganesh8999@github.com>
2020-07-08 18:57:21 +0530

    added toolbar component

--- burger-builder-starting-setup/src/components/Layout/Layout.js
+++ burger-builder-starting-setup/src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Auxi from "../../hoc/Auxi"
 import classes from "./Layout.css"
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 
 const layout = (props) => (
     <Auxi>
-        <div>Toolbaar sidebar backdrop</div>
+
+        <Toolbar />
         <main className={classes.Content}>
             {props.children}
         </main>
