901588704faa29944ea65ffc4d3370965e8327f0
Ganesh8999 <Ganesh8999@github.com>
2020-07-08 14:04:29 +0530

    calculating the ingredients sum dynamically

--- burger-builder-starting-setup/src/components/Burger/Burger.js
+++ burger-builder-starting-setup/src/components/Burger/Burger.js
@@ -6,13 +6,23 @@ import BurgerIngredients from "../Burger/BurgerIngredients/BurgerIngredients"
 
 const burger = (props) => {
 
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(ingredientKey => {
 
             return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
                 return <BurgerIngredients key={ingredientKey + i} type={ingredientKey} />
             })
-        })
+        }).reduce((arr, el) => {
+            return arr.concat(el);
+        }, [])
+
+    if (transformedIngredients.length === 0) {
+
+        transformedIngredients = <p>Please add more ingredients we can't serve just pieces of bread to our lovely customers</p>
+    }
+
+    console.log(transformedIngredients);
+
 
     return (
 
