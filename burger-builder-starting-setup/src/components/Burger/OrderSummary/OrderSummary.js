52b1ab085901e0dcb3d9c9456cdb3747004a867d
Ganesh8999 <Ganesh8999@github.com>
2020-07-08 17:31:05 +0530

    creating the order summary modal

---
+++ burger-builder-starting-setup/src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,33 @@
+import React from "react"
+import Auxi from "../../../hoc/Auxi"
+
+
+const orderSummary = (props) => {
+
+
+    const ingredientSummary = Object.keys(props.ingredients).map(ingrKey => {
+
+        return <li key={ingrKey}>
+            <span style={{ textTransform: 'capitalize' }}> {ingrKey}</span>
+             :
+              {props.ingredients[ingrKey]}
+        </li>
+    });
+
+
+    return (
+        <Auxi>
+            <h3>Your Order </h3>
+            <p> A delicious and amazing burger with the following arguments ,,Yummy :) </p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+
+
+        </Auxi>
+    )
+
+}
+
+
+export default orderSummary;
--- burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import Auxi from "../../hoc/Auxi";
-import Burger from "../../components/Burger/Burger"
-import BuildControls from "../../components/Burger/BuildControls/BuildControls"
+import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+import Modal from "../../components/UI/Modal/Modal";
+import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary"
+
 
 
 const INGREDIENTS_PRICE = {
@@ -99,6 +102,9 @@ class BurgerBuilder extends Component {
         return (
 
             <Auxi>
+                <Modal>
+                    <OrderSummary ingredients={this.state.ingredients} />
+                </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
