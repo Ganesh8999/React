7a0f1c989acae770579278dfcbd957b3b9f277b2
Ganesh8999 <Ganesh8999@github.com>
2020-07-09 13:33:11 +0530

    improvements in OrderSummary and Modal

--- burger-builder-starting-setup/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder-starting-setup/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,38 +1,48 @@
-import React from "react"
+import React, { Component } from "react"
 import Auxi from "../../../hoc/Auxi"
 import Button from "../../UI/Button/Button"
+import logo from "../../Logo/Logo";
 
 
-const orderSummary = (props) => {
+class OrderSummary extends Component {
+
+    componentDidUpdate() {
+        console.log("OrderSummary componentDidUpdate");
+
+    }
 
 
-    const ingredientSummary = Object.keys(props.ingredients).map(ingrKey => {
+    render() {
 
-        return <li key={ingrKey}>
-            <span style={{ textTransform: 'capitalize' }}> {ingrKey}</span>
-             :
-              {props.ingredients[ingrKey]}
-        </li>
-    });
+        const ingredientSummary = Object.keys(this.props.ingredients).map(ingrKey => {
+
+            return <li key={ingrKey}>
+                <span style={{ textTransform: 'capitalize' }}> {ingrKey}</span>
+                 :
+                  {this.props.ingredients[ingrKey]}
+            </li>
+        });
+
+        return (
+
+            <Auxi>
+                <h3>Your Order </h3>
+                <p> A delicious and amazing burger with the following arguments ,,Yummy :) </p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price : {this.props.price}</strong></p>
+                <p>COntinue to checkout</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>Cancel</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>Continue</Button>
 
 
-    return (
-        <Auxi>
-            <h3>Your Order </h3>
-            <p> A delicious and amazing burger with the following arguments ,,Yummy :) </p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price : {props.price}</strong></p>
-            <p>COntinue to checkout</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>Cancel</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>Continue</Button>
+            </Auxi>
+        )
 
-
-        </Auxi>
-    )
+    }
 
 }
 
 
-export default orderSummary;
+export default OrderSummary;
