4b5f837456e1ec27a419c2051718be849cb99ead
Ganesh8999 <Ganesh8999@github.com>
2020-07-09 10:56:43 +0530

    creating responsive sidedrawer

--- burger-builder-starting-setup/src/components/Layout/Layout.js
+++ burger-builder-starting-setup/src/components/Layout/Layout.js
@@ -2,12 +2,14 @@ import React from "react";
 import Auxi from "../../hoc/Auxi"
 import classes from "./Layout.css"
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import Sidedrawer from "../Navigation/Sidedrawer/Sidedrawer";
 
 
 const layout = (props) => (
     <Auxi>
 
         <Toolbar />
+        <Sidedrawer />
         <main className={classes.Content}>
             {props.children}
         </main>
