93e827c94d60992fe2dac375f4003f2d0343c65c
Ganesh8999 <Ganesh8999@github.com>
2020-07-11 16:26:19 +0530

    posting data to the server

--- http--01-starting-setup/src/components/NewPost/NewPost.js
+++ http--01-starting-setup/src/components/NewPost/NewPost.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from "axios";
 import './NewPost.css';
 
 class NewPost extends Component {
@@ -9,20 +9,34 @@ class NewPost extends Component {
         author: 'Max'
     }
 
-    render () {
+    postPostHandler = () => {
+
+        const postData = {
+            title: this.state.title,
+            body: this.state.body,
+            author: this.state.content,
+        };
+
+        axios.post('https://jsonplaceholder.typicode.com/posts', postData).then(response => {
+            console.log(response);
+        })
+
+    }
+
+    render() {
         return (
             <div className="NewPost">
                 <h1>Add a Post</h1>
                 <label>Title</label>
-                <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
+                <input type="text" value={this.state.title} onChange={(event) => this.setState({ title: event.target.value })} />
                 <label>Content</label>
-                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
+                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({ content: event.target.value })} />
                 <label>Author</label>
-                <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
-                    <option value="Max">Max</option>
-                    <option value="Manu">Manu</option>
+                <select value={this.state.author} onChange={(event) => this.setState({ author: event.target.value })}>
+                    <option value="Max">Ganesh Singh</option>
+                    <option value="Manu">Divya</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postPostHandler}>Add Post</button>
             </div>
         );
     }
