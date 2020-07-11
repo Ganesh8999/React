c0b0ac5b8d56e9bccb36613223f08335da65244d
Ganesh8999 <Ganesh8999@github.com>
2020-07-11 18:07:01 +0530

    axios more improvements

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import axios from "axios";
+
+// Removed bcz implemented in axios.js
+// import axios from "axios";
+
+import axios from "../../axios";
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
