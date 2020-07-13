4bc257c0bfe1c29d6ce6e55def59bd04d0b58f5e
Ganesh8999 <Ganesh8999@github.com>
2020-07-13 15:37:05 +0530

    rendering the components

--- routing-demo/src/containers/Blog/Blog.js
+++ routing-demo/src/containers/Blog/Blog.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Route } from "react-router";
 
 // Removed bcz implemented in axios.js
 // import axios from "axios";
 
 import axios from "../../axios";
 import Posts from "./Posts/Posts";
+import NewPost from "./NewPost/NewPost";
 import './Blog.css';
 
 class Blog extends Component {
@@ -21,8 +23,11 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home2</h1>} /> */}
 
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
             </div>
         );
     }
