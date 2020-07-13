27fa44c893cf6ed5a7bb76deee2282636eeb9555
Ganesh8999 <Ganesh8999@github.com>
2020-07-13 14:57:15 +0530

    added links in home page

--- routing-demo/src/containers/Blog/Blog.js
+++ routing-demo/src/containers/Blog/Blog.js
@@ -68,10 +68,14 @@ class Blog extends Component {
             })
         }
         return (
-            <div>
+            <div className="Blog">
                 <header>
                     <nav>
-                        <ul><li><a href="" /></li></ul>
+                        <ul>
+                            <li><a href="/" >Home</a></li>
+                            <li><a href="/new-post" >New Post</a></li>
+
+                        </ul>
                     </nav>
                 </header>
                 <section className="Posts">
