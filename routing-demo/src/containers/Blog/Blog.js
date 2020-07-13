ba566d05f42432a27745beab69814bab8785fbcf
Ganesh8999 <Ganesh8999@github.com>
2020-07-13 15:22:54 +0530

    added react-router-dom and prepared for routing

--- routing-demo/src/containers/Blog/Blog.js
+++ routing-demo/src/containers/Blog/Blog.js
@@ -4,69 +4,12 @@ import React, { Component } from 'react';
 // import axios from "axios";
 
 import axios from "../../axios";
-
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import Posts from "./Posts/Posts";
 import './Blog.css';
 
 class Blog extends Component {
-
-
-    state = {
-
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
-
-    componentDidMount() {
-
-        // axios.get('https://jsonplaceholder.typicode.com/posts').then(responseData => {
-        axios.get('/posts').then(responseData => {
-            //  console.log(responseData);
-            const posts = responseData.data.slice(0, 4);
-            const updatedPosts = posts.map(post => {
-
-                return {
-                    ...post,
-                    author: "Ganesh Singh"
-
-                }
-            })
-
-            this.setState({ posts: updatedPosts });
-
-
-
-        }).catch(error => {
-            // console.log(error);
-            this.setState({ error: true })
-        })
-    }
-
-    postSelectedHandler = (id) => {
-
-        this.setState({ selectedPostId: id });
-
-
-    }
-
     render() {
 
-        let posts = <p style={{ textAlign: 'center' }}> Something went wrong while fetching posts !! try again </p>
-
-        if (!this.state.error) {
-
-            posts = this.state.posts.map(post => {
-                return <Post
-                    key={post.id}
-                    title={post.title}
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)}
-                />
-            })
-        }
         return (
             <div className="Blog">
                 <header>
@@ -78,15 +21,8 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />;
-                </section>
+                <Posts />
+
             </div>
         );
     }
---
+++ routing-demo/src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import axios from "../../../axios";
+import Post from '../../../components/Post/Post';
+import './Posts.css';
+
+
+class Posts extends Component {
+
+
+    state = {
+
+        posts: []
+    }
+
+
+    componentDidMount() {
+
+        // axios.get('https://jsonplaceholder.typicode.com/posts').then(responseData => {
+        axios.get('/posts').then(responseData => {
+            //  console.log(responseData);
+            const posts = responseData.data.slice(0, 4);
+            const updatedPosts = posts.map(post => {
+
+                return {
+                    ...post,
+                    author: "Ganesh Singh"
+
+                }
+            })
+
+            this.setState({ posts: updatedPosts });
+
+
+
+        }).catch(error => {
+            // console.log(error);
+            // this.setState({ error: true })
+        })
+    }
+
+
+
+
+    postSelectedHandler = (id) => {
+
+        this.setState({ selectedPostId: id });
+
+
+    }
+
+    render() {
+
+
+        let posts = <p style={{ textAlign: 'center' }}> Something went wrong while fetching posts !! try again </p>
+
+        if (!this.state.error) {
+
+            posts = this.state.posts.map(post => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)}
+                />
+            })
+        }
+
+        return (
+
+            <section className="Posts">
+                {posts}
+            </section>
+
+        )
+    }
+}
+
+
+export default Posts;
