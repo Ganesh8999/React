acd79feb5d1d034397677ae382d39b32531703a5
Ganesh8999 <Ganesh8999@github.com>
2020-07-13 17:40:55 +0530

    switch to load single route

--- routing-demo/src/containers/Blog/Blog.js
+++ routing-demo/src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import { Route, Link } from "react-router-dom";
-import { Route, NavLink } from "react-router-dom";
+import { Route, NavLink, Switch } from "react-router-dom";
 
 // Removed bcz implemented in axios.js
 // import axios from "axios";
@@ -40,9 +40,11 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home2</h1>} /> */}
 
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} />
-                <Route path="/:id" exact component={FullPost} />
+                <Switch>
+                    <Route path="/" exact component={Posts} />
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/:id" exact component={FullPost} />
+                </Switch>
             </div>
         );
     }
