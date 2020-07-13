ab54d45f8fe6a384f22fe9d217d75b0534f50958
Ganesh8999 <Ganesh8999@github.com>
2020-07-13 14:40:40 +0530

    routing demo inital setup

---
+++ routing-demo/src/containers/Blog/Blog.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+
+// Removed bcz implemented in axios.js
+// import axios from "axios";
+
+import axios from "../../axios";
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+
+class Blog extends Component {
+
+
+    state = {
+
+        posts: [],
+        selectedPostId: null,
+        error: false
+    }
+
+    componentDidMount() {
+
+        // axios.get('https://jsonplaceholder.typicode.com/posts').then(responseData => {
+        axios.get('/posts').then(responseData => {
+            //  console.log(responseData);
+            const posts = responseData.data.slice(0, 4);
+            const updatedPosts = posts.map(post => {
+
+                return {
+                    ...post,
+                    author: "Ganesh Singh"
+
+                }
+            })
+
+            this.setState({ posts: updatedPosts });
+
+
+
+        }).catch(error => {
+            // console.log(error);
+            this.setState({ error: true })
+        })
+    }
+
+    postSelectedHandler = (id) => {
+
+        this.setState({ selectedPostId: id });
+
+
+    }
+
+    render() {
+
+        let posts = <p style={{ textAlign: 'center' }}> Something went wrong while fetching posts !! try again </p>
+
+        if (!this.state.error) {
+
+            posts = this.state.posts.map(post => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)}
+                />
+            })
+        }
+        return (
+            <div>
+                <header>
+                    <nav>
+                        <ul><li><a href="" /></li></ul>
+                    </nav>
+                </header>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <section>
+                    <FullPost id={this.state.selectedPostId} />
+                </section>
+                <section>
+                    <NewPost />;
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog; 
