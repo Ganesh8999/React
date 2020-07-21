7a6205b262d7755b9df3c451752cbe591eabc456
Ganesh8999 <Ganesh8999@github.com>
2020-07-21 15:34:08 +0530

    replaced if's with switch case

--- redux-demo-start/src/store/reducer.js
+++ redux-demo-start/src/store/reducer.js
@@ -5,31 +5,30 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
 
-    if (action.type === "INCREMENT") {
 
-        return {
-            counter: state.counter + 1
-        }
+    switch (action.type) {
+        case "INCREMENT":
+            return {
+                counter: state.counter + 1
+            }
+        case "DECREMENT":
+            return {
+                counter: state.counter - 1
+            }
+        case "ADDITION":
+            return {
+                counter: state.counter + action.val
+            }
+        case "SUBTRACTION":
+            return {
+                counter: state.counter - action.val
+            }
+
+        default:
+            break;
+
+
     }
-    if (action.type === "DECREMENT") {
-
-        return {
-            counter: state.counter - 1
-        }
-    }
-    if (action.type === "ADDITION") {
-
-        return {
-            counter: state.counter + action.val
-        }
-    }
-    if (action.type === "SUBTRACTION") {
-
-        return {
-            counter: state.counter - action.val
-        }
-    }
-
     return state;
 }
 
