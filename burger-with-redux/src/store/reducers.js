e7cf960bae5876d697ead2a62673aa6e61b7a2d5
Ganesh8999 <Ganesh8999@github.com>
2020-07-22 12:39:11 +0530

    display totalPrice calculation

--- burger-with-redux/src/store/reducers.js
+++ burger-with-redux/src/store/reducers.js
@@ -12,6 +12,15 @@ const initialState = {
 }
 
 
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+};
+
+
 const reducer = (state = initialState, action) => {
 
     switch (action.type) {
@@ -23,7 +32,8 @@ const reducer = (state = initialState, action) => {
                 ingredients: {
                     ...state.ingredients,
                     [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-                }
+                },
+                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
 
             };
         case actionType.REMOVE_INGREDIENT:
@@ -34,7 +44,9 @@ const reducer = (state = initialState, action) => {
                 ingredients: {
                     ...state.ingredients,
                     [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-                }
+                },
+                totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
+
             };
 
         default:
