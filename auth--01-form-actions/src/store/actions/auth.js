b9dc3e327764f361abd195d57b8cd488de4e33e1
Ganesh8999 <Ganesh8999@github.com>
2020-07-27 15:02:27 +0530

    accessing the protected resources

--- auth--01-form-actions/src/store/actions/auth.js
+++ auth--01-form-actions/src/store/actions/auth.js
@@ -22,6 +22,27 @@ export const authFail = (error) => {
     };
 };
 
+export const logout = () => {
+
+    return {
+        type: actionTypes.LOGOUT,
+        error: null,
+        expiresIn: null
+    }
+}
+
+export const checkAuthTimeout = (expiresIn) => {
+
+    return dispatch => {
+
+        setTimeout(() => {
+
+            dispatch(logout());
+
+        }, expiresIn * 1000);
+    }
+}
+
 export const auth = (email, password, isSignUp) => {
     return dispatch => {
         dispatch(authStart());
@@ -44,6 +65,7 @@ export const auth = (email, password, isSignUp) => {
             .then(response => {
                 console.log(response);
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn));
 
             })
             .catch(err => {
--- auth--01-form-actions/src/store/reducers/auth.js
+++ auth--01-form-actions/src/store/reducers/auth.js
@@ -27,15 +27,25 @@ const authFail = (state, action) => {
 
 }
 
+const logout = (state, action) => {
+
+    return updateObject(state, {
+        token: null,
+        userId: null
+    })
+}
+
 
 const reducer = (state = initialState, action) => {
 
     switch (action.type) {
-        case actionTypes.AUTH_START: return authStart
+        case actionTypes.AUTH_START: return authStart(state, action)
 
-        case actionTypes.AUTH_SUCCESS: return authSuccess
+        case actionTypes.AUTH_SUCCESS: return authSuccess(state, action)
 
-        case actionTypes.AUTH_FAIL: return authFail
+        case actionTypes.AUTH_FAIL: return authFail(state, action)
+
+        case actionTypes.LOGOUT: return logout(state, action)
 
 
 
