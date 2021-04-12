ae105e1072dc6f365b4284ded5937085b550c250
Ganesh8999 <Ganesh8999@github.com>
2021-04-13 04:39:51 +0530

    controllesd comps demo added

---
+++ ConceptsDemoCode/ControlledComponents.js
@@ -0,0 +1,8 @@
+// A component that controls the input elements within the forms on subsequent user input is called Controlled Component,
+// i.e, every state mutation will have an associated handler function.
+
+// Demo code
+
+// handleChange(e){
+// this.setState({value : e.target.value.toUpperCase()})
+// }
