2529b2a1092f2d57301eb902e02f52ddb07649e1
Ganesh8999 <Ganesh8999@github.com>
2021-04-13 04:40:22 +0530

    props proxy for HOC

---
+++ ConceptsDemoCode/PropsProxyForHOC.js
@@ -0,0 +1,14 @@
+function HOC(WrappedComponent) {
+  return class Test extends Component {
+    render() {
+      const newProps = {
+        title: "New Header",
+        footer: false,
+        showFeatureX: false,
+        showFeatureY: true,
+      };
+
+      return <wrappedComponent {...this.props} {...newProps} />;
+    }
+  };
+}
