4ed7f5331d778eff15d1726c054217a0ef25ca19
Ganesh8999 <Ganesh8999@github.com>
2021-04-15 05:43:50 +0530

    freagments demo

---
+++ ConceptsDemoCode/Fragments.js
@@ -0,0 +1,27 @@
+// render(){
+//     return (
+//         <React.Fragment>
+//             <G/>
+//             <A/>
+//             <D/>
+//             </>
+//             <C/>
+//             <B/>
+//         </React.Fragment>
+//     )
+// }
+
+// Shorthand syntax
+
+// render(){
+//     return (
+//         <>
+//             <G/>
+//             <A/>
+//             <D/>
+//             </>
+//             <C/>
+//             <B/>
+//         <>
+//     )
+// }
