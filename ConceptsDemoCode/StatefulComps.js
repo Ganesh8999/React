ea1c0e21e46b9109ddd70f5dc9bc9af85e4a93fa
Ganesh8999 <Ganesh8999@github.com>
2021-04-15 05:48:52 +0530

    stateful comps

---
+++ ConceptsDemoCode/StatefulComps.js
@@ -0,0 +1,29 @@
+// class App extends React.Component {
+//   constructor() {
+//     super(props);
+
+//     this.state = { count: 0 };
+//   }
+
+//   render() {
+//     //
+//     //
+//   }
+// }
+
+
+// after 16.8 update
+// The equivalent functional component
+
+
+import React , {useState} from 'react';
+
+
+const App = (props) => {
+
+    const [count, setCount] = useState(0);
+
+    return (
+        // JSX
+    )
+}
