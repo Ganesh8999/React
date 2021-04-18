b03ed8e947c33037f33a17b86d5f8de9d421f7ee
Ganesh8999 <Ganesh8999@github.com>
2021-04-18 21:24:03 +0530

    just removed extra spaces

--- burger-basics--06-finished/src/axios-orders.js
+++ burger-basics--06-finished/src/axios-orders.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-
 const instance = axios.create({
-
-    baseURL: 'https://react-burger-project-2231f.firebaseio.com/'
+  baseURL: "https://react-burger-project-2231f.firebaseio.com/",
 });
-
 
 export default instance;
