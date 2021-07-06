5e8d83ced2a320e5bcca5be819dc029ec4520039
Ganesh8999 <Ganesh8999@github.com>
2021-07-06 14:27:25 +0530

    added firebase config

---
+++ ReactProjects/amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB_w4MdgxGTv53_y5OJne-NoSgkRqvs82o",
+    authDomain: "ecommerce-638a2.firebaseapp.com",
+    projectId: "ecommerce-638a2",
+    storageBucket: "ecommerce-638a2.appspot.com",
+    messagingSenderId: "380816803356",
+    appId: "1:380816803356:web:941f02ec4726459c9801d9",
+    measurementId: "G-P7R3XDLH5G"
+  };
