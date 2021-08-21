7c66ef647e8ec21ad12f6995c4e78edacae21b97
Ganesh8999 <Ganesh8999@github.com>
2021-08-21 15:31:32 +0530

    added useEffect and firebase

--- ReactProjects/amazon-clone/src/App.js
+++ ReactProjects/amazon-clone/src/App.js
@@ -1,21 +1,54 @@
 import "./App.css";
+import { useEffect } from "react";
 import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Checkout from "./Checkout";
+import Login from "./Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // will only run once when the app component loads
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("the user is  <<<<< ", authUser);
+
+      if (auth) {
+        // the user just logged in or already logged
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
+  // BEM convention
   return (
-    // BEM convention
     <Router>
       <div className="app">
-        <Header />
         <Switch>
+          <Route path="/login">
+            {/* <h1>Login page</h1> */}
+            <Login />
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
